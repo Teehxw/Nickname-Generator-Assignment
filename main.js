986070ae65c1fc6bb6b69a7ecd2ba11dbc23961c
Teehxw <112659668+Teehxw@users.noreply.github.com>
2022-10-11 13:37:30 -0600

    blah blah

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 // Nickname Generator 
 
 //GlobaL Variables     
-let containerE1 = dpcument.getElementById("container");
-let firstE1 = document.getElementById("first");
-let lastE1 = document.getElementById("last");
+let containerE1 = document.getElementById("container");
 let randNickE1=document.getElementById("random");
 let allNickE1 =  document.getElementById("all")
 
@@ -11,3 +9,22 @@ let allNickE1 =  document.getElementById("all")
 randNickE1.addEventListener("click", randomNick);
 allNickE1.addEventListener("click", allNicks);
 
+// Array
+let nicknames = ["twinkle-toes", "beautiful", "fortnite-sweat", "shadow", "faze", "da gay", "da perv", "ismylov"];
+// index ->            0              1               2             3        4        5         6           7
+
+//Process
+
+function randomNick(){
+    //Global Variables
+    let first = document.getElementById("first").value;
+    let last = document.getElementById("last").value;
+    
+    //output
+    let randNickname = randomElement(nicknames)
+    containerE1.innerHTML = `${first} "${randNickname}"${last}`;
+}
+
+function allNicks() {
+
+}
